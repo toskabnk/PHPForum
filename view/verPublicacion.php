7331ddc5c33cc7492f84548bfc14e5e3132810b3
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2023-04-28 23:08:42 +0200

    Error publicacion que no existe - Si pones un id de una publicacion que no existe ahora te pone un mensaje. Antes saltaba un error.

--- view/verPublicacion.php
+++ view/verPublicacion.php
@@ -2,34 +2,44 @@
 include("header.php");
 include_once '../controller/publicacionesController.php';
 include_once '../controller/mensajesController.php';
+if ($publicacion != null){
 ?>
 <div class="container-sm text-center justify-content-center w-25 mt-5">
-        <div class="row justify-content-center mb-1">
-            <div class="col">
-                Titulo publicacion:<?php echo $publicacion->getTitulo(); ?></td>
-            </div>
-            <div class="col">
-                Mensaje:<?php echo $publicacion->getMensaje(); ?></td>
-            </div>
+    <div class="row justify-content-center mb-1">
+        <div class="col">
+            Titulo publicacion:<?php echo $publicacion->getTitulo(); ?></td>
         </div>
+        <div class="col">
+            Mensaje:<?php echo $publicacion->getMensaje(); ?></td>
+        </div>
+    </div>
 </div>
 <div class="container-sm text-center justify-content-center w-25 mt-5">
     <?php
-    if($mensajes != null){
-    foreach ($mensajes as $tem): ?>
+    if ($mensajes != null) {
+        foreach ($mensajes as $tem): ?>
+            <div class="row justify-content-center mb-1">
+                <div class="col">
+                    Usuario:<?php echo $userController->consigueNombre($tem['userId'])["nombre_usuario"] ?></td>
+                </div>
+                <div class="col">
+                    Mensaje:<?php echo $tem['mensaje'] ?></td>
+                </div>
+            </div>
+        <?php endforeach;
+    } else {
+        ?>
         <div class="row justify-content-center mb-1">
-            <div class="col">
-                Usuario:<?php echo $userController->consigueNombre($tem['userId'])["nombre_usuario"] ?></td>
-            </div>
-            <div class="col">
-                Mensaje:<?php echo $tem['mensaje'] ?></td>
-            </div>
+            Aun no hay respuestas
         </div>
-    <?php endforeach;} else {
-    ?><div class="row justify-content-center mb-1">
-        Aun no hay respuestas
-        </div><?php
-    } ?>
+        <?php
+    }
+    } else {
+        ?>
+        <div class="container-sm text-center justify-content-center w-25 mt-5">
+            No existe esa publicacion
+        </div>
+    <?php } ?>
 </div>
 </body>
 </html>
